d8a3e1e7597f85ec798990aa80bd6eecd376bfcc
Marc Widmer <marc@pbi.io>
2025-05-29 13:19:48 +0200

    feat: add View restrictions and Page permissions support

--- unfold_extra/contrib/cms/admin.py
+++ unfold_extra/contrib/cms/admin.py
@@ -4,14 +4,17 @@ from cms.admin.pageadmin import PageAdmin as BasePageAdmin
 from cms.admin.pageadmin import PageContentAdmin as BasePageContentAdmin
 from cms.admin.permissionadmin import (
     GlobalPagePermissionAdmin as BaseGlobalPagePermissionAdmin,
+    ViewRestrictionInlineAdmin,
+    PagePermissionInlineAdmin,
 )
 from cms.admin.useradmin import (
     PageUserAdmin,
     PageUserGroupAdmin as BasePageUserGroupAdmin,
 )
 from cms.models import GlobalPagePermission, Page, PageContent, PageUser, PageUserGroup
-from unfold.admin import ModelAdmin
+from unfold.admin import ModelAdmin, TabularInline
 from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
+from cms.utils.conf import get_cms_setting
 
 from .forms import (
     AddPageForm,
@@ -44,6 +47,23 @@ class PageUserGroupAdmin(PageUserAdmin, ModelAdmin):
 class GlobalPagePermissionAdmin(BaseGlobalPagePermissionAdmin, ModelAdmin):
     compressed_fields = True
 
+class UnfoldViewRestrictionInlineAdmin(ViewRestrictionInlineAdmin, TabularInline):
+    tab = True
+    autocomplete_fields = ["user", "group"]
+
+class UnfoldVPagePermissionInlineAdmin(PagePermissionInlineAdmin, TabularInline):
+    tab = True
+
+
+UNFOLD_PERMISSION_ADMIN_INLINES = []
+if get_cms_setting('PERMISSION'):
+    admin.site.unregister(GlobalPagePermission)
+    admin.site.register(GlobalPagePermission, GlobalPagePermissionAdmin)
+    UNFOLD_PERMISSION_ADMIN_INLINES.extend([
+        UnfoldViewRestrictionInlineAdmin,
+        UnfoldVPagePermissionInlineAdmin,
+    ])
+
 
 @admin.register(PageContent)
 class PageContentAdmin(BasePageContentAdmin, ModelAdmin):
@@ -68,6 +88,7 @@ class PageAdmin(BasePageAdmin, ModelAdmin):
     form = AdvancedSettingsForm
 
     compressed_fields = True
+    inlines = UNFOLD_PERMISSION_ADMIN_INLINES
 
     def __init__(self, model, admin_site):
         super().__init__(model, admin_site)
