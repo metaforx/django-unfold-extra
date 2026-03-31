9fb246949cffd2cabbbda306b33ae6b7a990ccc6
stahl <office@stahlnow.com>
2026-03-31 19:31:51 +0200

    django-versatileimagefield contrib

---
+++ unfold_extra/contrib/versatileimagefield/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VersatileimagefieldConfig(AppConfig):
+    name = "unfold_extra.contrib.versatileimagefield"
+    label = "unfold_extra_versatileimagefield"
