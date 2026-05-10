9400ec6e17327011494c56d2b827745221038b5f
Marc Widmer <marc@pbi.io>
2026-05-10 09:01:13 +0200

    fix: ensure stock Site admin registers before custom unregister/re-register

--- unfold_extra/contrib/sites/admin.py
+++ unfold_extra/contrib/sites/admin.py
@@ -1,3 +1,5 @@
+import django.contrib.sites.admin  # noqa: F401  -- ensure stock Site admin registers before our unregister/re-register
+
 from django.contrib import admin
 from django.contrib.sites.models import Site
 
